34b397b2a0840ba8243c5bac0b1d4cd7016c6757
Ines Daniel <i.danie8@gmail.com>
2018-08-19 12:01:16 -0500

    add student id to experiences table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_19_163152) do
+ActiveRecord::Schema.define(version: 2018_08_19_165927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "capstones", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "url"
+    t.string "screenshot"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "experiences", force: :cascade do |t|
     t.datetime "start_date"
@@ -21,6 +30,13 @@ ActiveRecord::Schema.define(version: 2018_08_19_163152) do
     t.string "job_title"
     t.string "complany_name"
     t.text "details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "student_id"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "skill_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
