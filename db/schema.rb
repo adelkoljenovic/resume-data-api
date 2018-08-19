4db197974ddf835311b1f9b807456b1845a86900
Ines Daniel <i.danie8@gmail.com>
2018-08-19 12:15:00 -0500

    change complany to company

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_19_165927) do
+ActiveRecord::Schema.define(version: 2018_08_19_171153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2018_08_19_165927) do
     t.datetime "start_date"
     t.datetime "end_date"
     t.string "job_title"
-    t.string "complany_name"
+    t.string "company_name"
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
