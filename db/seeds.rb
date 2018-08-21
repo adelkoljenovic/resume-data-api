e9a6e5a899712cedd8eeff6840217a7fc5e2f2ee
Heather Cleland <heathercleland@gmail.com>
2018-08-20 19:51:45 -0500

    fix date in education table

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-education1 = Education.new(start_date: 2005-06-23, end_date: 2010-8-12, degree: "Bachelor of Arts", university_name: "University of Texas", details: "Real-time; strategize webservices leverage action-items synergies widgets enable generate. Integrate ad delivery harness design extend remix enhance cross-media. Granular, extend weblogs, integrate; sexy expedite e-business drive markets interactive")
+education1 = Education.new(start_date: "2005-06-23", end_date: "2010-8-12", degree: "Bachelor of Arts", university_name: "University of Texas", details: "Real-time; strategize webservices leverage action-items synergies widgets enable generate. Integrate ad delivery harness design extend remix enhance cross-media. Granular, extend weblogs, integrate; sexy expedite e-business drive markets interactive")
 education1.save
 
-education1 = Education.new(start_date: 2003-03-14, end_date: 2011-4-28, degree: "Bachelor of Science", university_name: "University of South Dakota", details: "Real-time; strategize webservices leverage action-items synergies widgets enable generate. Integrate ad delivery harness design extend remix enhance cross-media. Granular, extend weblogs, integrate; sexy expedite e-business drive markets interactive")
+education1 = Education.new(start_date: "2003-03-14", end_date: "2011-4-28", degree: "Bachelor of Science", university_name: "University of South Dakota", details: "Real-time; strategize webservices leverage action-items synergies widgets enable generate. Integrate ad delivery harness design extend remix enhance cross-media. Granular, extend weblogs, integrate; sexy expedite e-business drive markets interactive")
 education1.save
 
-education1 = Education.new(start_date: 2014-10-17, end_date: 2018-2-3, degree: "Bachelor of Science", university_name: "Miami University", details: "Real-time; strategize webservices leverage action-items synergies widgets enable generate. Integrate ad delivery harness design extend remix enhance cross-media. Granular, extend weblogs, integrate; sexy expedite e-business drive markets interactive")
+education1 = Education.new(start_date: "2014-10-17", end_date: "2018-2-3", degree: "Bachelor of Science", university_name: "Miami University", details: "Real-time; strategize webservices leverage action-items synergies widgets enable generate. Integrate ad delivery harness design extend remix enhance cross-media. Granular, extend weblogs, integrate; sexy expedite e-business drive markets interactive")
 education1.save
