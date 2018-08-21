1c169f08611d41ef6f8dfec6e34d10aa3b8e233a
Adel Koljenovic <adel.koljenovic@outlook.com>
2018-08-20 21:40:34 -0400

    add capstones and skills to the seeds file - tested

--- db/schema.rb
+++ db/schema.rb
@@ -46,27 +46,6 @@ ActiveRecord::Schema.define(version: 2018_08_19_171321) do
     t.integer "student_id"
   end
 
-  create_table "educations", force: :cascade do |t|
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.string "degree"
-    t.string "university_name"
-    t.text "details"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "experiences", force: :cascade do |t|
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.string "job_title"
-    t.string "complany_name"
-    t.text "details"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "student_id"
-  end
-
   create_table "skills", force: :cascade do |t|
     t.string "skill_name"
     t.datetime "created_at", null: false
